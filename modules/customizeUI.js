e2b7b26febee391bdfedbd8e5f7a7ce48047210c
alex123bob <alex123bob@126.com>
2020-05-14 18:06:43 +0800

    change background into background color

--- modules/customizeUI.js
+++ modules/customizeUI.js
@@ -1,5 +1,5 @@
 chrome.storage.sync.get(['color'], result => {
-    let css = result.color ? `#root .chr-Site .alm-header-container .alm-header { background: ${result.color}; }` : ''
+    let css = result.color ? `#root .chr-Site .alm-header-container .alm-header { background-color: ${result.color}; }` : ''
     // let css = `.classic .chr-QuickDetailQuickDetailEditor, .rng .chr-QuickDetailQuickDetailEditor { background: ${result.color}; }`
     const head = document.head || document.getElementsByTagName('head')[0]
     let style = document.createElement('style')
