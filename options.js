6aff761f1fa91f8d500c3ac3b449eb8f3ca3e04d
alex123bob <alex123bob@126.com>
2020-05-15 14:05:41 +0800

    Add more theme color into palette

--- options.js
+++ options.js
@@ -1,5 +1,7 @@
 let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1', '#5596e6', '#3d70b2', '#41d6c3', '#42b0e2', '#261e1e', '#b7714b']
+const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1', '#5596e6', '#3d70b2', 
+'#41d6c3', '#42b0e2', '#261e1e', '#b7714b', '#FF8C94', '#2F9599', '#594F4F',
+'#A7226E', '#EC2049', '#F26B38', '#F7DB4F', '#355C7D', '#A8A7A7']
 function constructOptions(kButtonColors) {
     for (let item of kButtonColors) {
         let button = document.createElement('button')
