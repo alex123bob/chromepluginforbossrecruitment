5d14264daa9f86ebdaaa2527e0fb24d83a73190b
alex123bob <alex123bob@126.com>
2020-05-15 15:56:15 +0800

    bolden border of invalid fields

--- modules/customizeUI.js
+++ modules/customizeUI.js
@@ -3,6 +3,9 @@ chrome.storage.sync.get(['color'], result => {
     if (result.color) {
         css = `#root .chr-Site .alm-header-container .alm-header, div#footer, .classic .chr-NavDropMenu { background-color: ${result.color}; }
         .classic #root .project-nav-container:hover {background-color: ${result.color + '80'};}
+        .classic #root .chr-QuickDetailAttributeEditorWrapper.is-invalid .smb-Select-trigger {border-width: 2px;}
+        .classic #root .chr-QuickDetailAttributeEditorWrapper.is-invalid .smb-TextInput {border-width: 2px;}
+        .classic #root .chr-QuickDetailAttributeEditorWrapper.is-invalid .smb-TextInput.is-invalid {border-width: 2px;}
         `
     }
     else {
